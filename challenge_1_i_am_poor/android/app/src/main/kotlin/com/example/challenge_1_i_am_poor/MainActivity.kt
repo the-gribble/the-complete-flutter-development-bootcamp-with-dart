f08381baa1b00f858b99a89ec5019d711580df40
Sam <74820069+the-gribble@users.noreply.github.com>
2024-07-03 17:32:40 +0200

    Completed first challenge in VS Code: I Am Poor
    Did this one in VS Code. I'll do all challenges in VS Code going forward.

---
+++ challenge_1_i_am_poor/android/app/src/main/kotlin/com/example/challenge_1_i_am_poor/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.challenge_1_i_am_poor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ challenge_1_i_am_poor/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: const Text(
+            'I Am Poor',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: const Center(
+        child: Image(
+          image: AssetImage('images/coal.jpg'),
+        ),
+      ),
+    ),
+  ));
+}
